d377165477bec926871ab99727990057f49336bf
Estevão Henrique Firmiano <34587163+efirmiano@users.noreply.github.com>
2024-04-18 23:03:28 -0300

    Primeiro Commit do Projeto JavaSpring MVC

---
+++ src/main/java/br/com/Program3/Program3Application.java
@@ -0,0 +1,13 @@
+package br.com.Program3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Program3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Program3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/Program3/Program3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Program3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Program3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
